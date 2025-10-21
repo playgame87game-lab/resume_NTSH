750f97bd2c05d3334648f546cdcf40a85f7605fa
playgame87game-lab <playgame87game@gmail.com>
2025-10-21 11:47:08 +0800

    Rename Flask app and template in app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__劉子謙__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('30923劉子謙')
 
 @app.route('/competition')
 def competition():
